4a615ff1b1b90f4b527a1572da0edf51318435f6
Yang Yang <asceyang@me.com>
2018-11-30 00:20:37 +0900

    using pytest to run test

--- linked_list.py
+++ linked_list.py
@@ -98,11 +98,3 @@ def test_len():
     assert len(linkedList) == 3
 
     return linkedList
-
-
-test_append()
-test_remove_first_node()
-test_remove_last_node()
-test_remove_middle_node()
-test_remove_non_exist_node()
-test_len()
