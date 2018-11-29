77c141d77f0aaa7f9a64c87b417f68f99620844e
Yang Yang <asceyang@me.com>
2018-11-30 00:10:43 +0900

    added linked list append & remove & len function

---
+++ linked_list.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+class Node(object):
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+    
+    def __str__(self):
+        return "<value: {}>".format(self.value)
+
+    __repr__ = __str__
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = Node()
+        self.length = 0
+
+    def append(self, value):
+        currentNode = self.head
+        while currentNode.next != None:
+            currentNode = currentNode.next
+        currentNode.next = Node(value)
+        self.length = self.length + 1
+
+    def remove(self, value):
+        prevNode = self.head
+        currentNode = self.head.next
+        while currentNode != None and currentNode.value != value:
+            prevNode = currentNode
+            currentNode = currentNode.next
+        if currentNode != None:
+            prevNode.next = currentNode.next
+            self.length = self.length - 1
+    
+    def iter_node(self):
+        currentNode = self.head.next
+        while currentNode != None:
+            yield currentNode
+            currentNode = currentNode.next
+    
+    def __iter__(self):
+        for node in self.iter_node():
+            yield node.value
+    
+    def __len__(self):
+        return self.length
+
+
+def test_append():
+    linkedList = LinkedList()
+    linkedList.append(2)
+    linkedList.append(1)
+    linkedList.append(3)
+    linkedList.append(0)
+
+    return linkedList
+
+
+def test_remove_first_node():
+    linkedList = test_append()
+    linkedList.remove(2)
+    
+    assert list(linkedList) == [1, 3, 0]
+    return linkedList
+
+
+def test_remove_last_node():
+    linkedList = test_append()
+    linkedList.remove(0)
+    
+    assert list(linkedList) == [2, 1, 3]
+    return linkedList
+
+
+def test_remove_middle_node():
+    linkedList = test_append()
+    linkedList.remove(3)
+    
+    assert list(linkedList) == [2, 1, 0]
+    return linkedList
+
+
+def test_remove_non_exist_node():
+    linkedList = test_append()
+    linkedList.remove(100)
+    
+    assert list(linkedList) == [2, 1, 3, 0]
+    return linkedList
+
+
+def test_len():
+    linkedList = test_append()
+    assert len(linkedList) == 4
+    linkedList.remove(1)
+    assert len(linkedList) == 3
+    linkedList.remove(100)
+    assert len(linkedList) == 3
+
+    return linkedList
+
+
+print(list(test_append()))
+print(list(test_remove_first_node()))
+print(list(test_remove_last_node()))
+print(list(test_remove_middle_node()))
+print(list(test_remove_non_exist_node()))
+test_len()
